b65cd9a2f4ddd606debc026bf3681a981b0d3dce
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-06-03 18:57:37 -0400

    added model codes for sponsor and user

--- backend/models/sponsorModel.js
+++ backend/models/sponsorModel.js
@@ -1,1 +1,25 @@
 //sponsor sign up form schema
+const mongoose = require("mongoose");
+
+// User Schema
+const sponsorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  addedUsers: {
+    type: Array,
+    required: false,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create a model for a User
+module.exports = mongoose.model("sponsor", sponorSchema);
