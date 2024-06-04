b182f640ca753df4d8fbe605e6d26acae9bd1db1
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2024-06-03 20:15:53 -0400

    Auth backend Login + Register + db.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-//spin up server that connects to each route and mongo db
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { connect } = require('./db.js');
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+app.use(express.static('    public'));
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/users', authRoutes);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    connect();
+    console.log(`Server is running on port ${PORT}`);
+  });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,9 @@
 //login and register routes 
+const express = require('express');
+const router = express.Router();
+const userControllers = require('../controllers/authControllers.js')
+
+ router.post('/login', userControllers.loginUser)
+
+router.post('/register', userControllers.registerNewUser)
+module.exports = router;
