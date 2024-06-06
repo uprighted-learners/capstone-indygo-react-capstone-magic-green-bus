dff3a5d8e0246ef38a4fb5893cffbbc87489fde4
alexaubin-png <157660617+alexaubin-png@users.noreply.github.com>
2024-06-05 20:56:08 -0400

    Finished Auth Crud, Login || Register || TODO: FRONTEND FETCH TO LOGIN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import Auth from "./components/Auth";
+import Navigation from "./components/Nav.js";
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div>
+          <Auth />
+          <Routes>        
+            <Route path="/login" element={<Auth />} />
+          </Routes>
+      </div>
+    </Router>
+  );
 }
