9767418dfd2e080e7378ef3f4711a331a7028681
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-06-10 18:28:55 -0400

    added sponsor routes

--- backend/routes/sponsorRoutes.js
+++ backend/routes/sponsorRoutes.js
@@ -1,15 +1,13 @@
 //form sign up routes
 
 //get all sign ups - get - /sponsor/all
-
+router.get("/all", sponsorController.getAllSponsors);
 
 //get sign up by id - get - /sponsor/:id
 
-
 //create sign up - post - /sponsor/create
-
 
 //update sign up - put - /sponsor/update/:id
 
-
 //delete sign up - delete - /sponsor/delete/:id
+``;
