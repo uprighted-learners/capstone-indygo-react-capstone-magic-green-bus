01f3f442e5746f22a511231ee9bcfb8c77826099
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-07-01 19:42:01 -0400

    testing font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home.js'
-import NavBar from './NavBar';
-import About from './components/About';
-import Auth from './components/Auth';
-import Contact from './components/Contact';
-import Sponsor from './components/Sponsor';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.js";
+import NavBar from "./NavBar";
+import About from "./components/About";
+import Auth from "./components/Auth";
+import Contact from "./components/Contact";
+import Sponsor from "./components/Sponsor";
+import Footer from "./components/Footer";
 
-import Map from './components/Map';
+import Map from "./components/Map";
 
 function App() {
+  // const textstyle = {
+  //   fontsize: "16px",
+  // };
+  // return <div style={textstyle}>Hello World</div>;
+  // }
   return (
     <Router>
       <NavBar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/sponsor' element={<Sponsor />} />
-        <Route path='/map' element={<Map />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/sponsor" element={<Sponsor />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
       <Footer />
     </Router>
@@ -29,4 +34,3 @@ function App() {
 }
 
 export default App;
-
