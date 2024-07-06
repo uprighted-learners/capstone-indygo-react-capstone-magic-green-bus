82f434439b90d391852da250790351617a8f8326
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-07-05 20:23:22 -0400

    added styling, changed bold letters

--- frontend/src/components/Sponsor.js
+++ frontend/src/components/Sponsor.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import './Sponsor.css';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import "./Sponsor.css";
 
 export default function SponsorRegister() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const location = useLocation(); // Retrieve selected location from previous page (map)
   const [locationInput, setLocationInput] = useState("");
   const selectedLocation = location.state?.location; // Get selected location from state from map.js
@@ -19,27 +19,27 @@ export default function SponsorRegister() {
     e.preventDefault();
     try {
       const response = await fetch(
-        'http://localhost:8080/sponsor/create', //go check registerSponsor in backend/routes/sponsorRoutes to make sure names match.js
+        "http://localhost:8080/sponsor/create", //go check registerSponsor in backend/routes/sponsorRoutes to make sure names match.js
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             name,
             location: locationInput,
           }),
-        },
+        }
       );
       console.log(response);
       if (response.ok) {
-        setName('');
-        setLocationInput('');
+        setName("");
+        setLocationInput("");
         alert("Sponsor created successfully!");
       } else if (response.status === 409) {
-        alert('Location already sponsored!');
+        alert("Location already sponsored!");
       } else {
-        alert('Failed to sponsor, please check information');
+        alert("Failed to sponsor, please check information");
       }
     } catch (error) {
       alert(error.message);
@@ -47,49 +47,53 @@ export default function SponsorRegister() {
   };
 
   return (
-<>
-    
-    <div className='sponsor-form-container'>
-
-      <label className='sponsor-form-container2'>Sponsor a stop</label>
-
- 
-      <h2 className='New-Sponsor'>Register New Sponsor</h2>
-      <br></br>
-      <form onSubmit={handleSubmit}>
-        <label className='nameLabel-Sponsor'>Name:</label>
-        <input className='NameInput-sponsor'
-          list="name-options"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-          <br></br>
-          <label>Location:</label>
-          <input
-            type='location'
-            value={locationInput}
-            onChange={(e) => setLocationInput(e.target.value)}
-            required
-          />
-          <br></br>
-          {localStorage.getItem("token") ? (
-            <button onChange={console.log("hi")}> REGISTER</button> 
-          ) : (
-            <p>You must be logged in to view this button</p>
-          )}
-        </form>
+    <>
+      <div className="sponsor-form-container">
+        <h2 className="New-Sponsor">Register New Sponsor</h2>
         <br></br>
-        <p>
-          Sponsorship responsibilities will be held for 1 month, feel free to
-          responsor after that.
-        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="labels">
+            <label className="nameLabel-Sponsor">Name:</label>
+            <input
+              className="NameInput-sponsor"
+              list="name-options"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <br></br>
+            <label className="locationLabel-Sponsor">Location:</label>
+            <input
+              type="location"
+              value={locationInput}
+              onChange={(e) => setLocationInput(e.target.value)}
+              required
+            />
+          </div>
+          <br></br>
+          <div className="Register-buttons">
+            {localStorage.getItem("token") ? (
+              <button onChange={console.log("hi")}> REGISTER</button>
+            ) : (
+              <p>You must be logged in to view this button</p>
+            )}
+          </div>
+        </form>
+
+        <br></br>
+        <div className="Sponsor-Time">
+          <p>
+            Sponsorship responsibilities will be held for 1 month, feel free to
+            responsor after that.
+          </p>
+        </div>
+        <br></br>
         <img
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVrsXtoBS6GbuRmp_-W0FftkzFOl9FkJrJWQ&s'
-          alt='IndyGo Logo'
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVrsXtoBS6GbuRmp_-W0FftkzFOl9FkJrJWQ&s"
+          alt="IndyGo Logo"
         />
-        <p>
+        <p className="lorem-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -105,14 +109,17 @@ export default function SponsorRegister() {
           voluptatem sequi nesciunt. Neque porro quisquam
         </p>
         <div className="sponsor-pic">
-          <h1>SPONSOR A STOP</h1>
           <br></br>
-          <p>
-            take the the take the time to sponsor a bus stop near you NOW!!!.
-            With the value of your information that allow us to collect data,
-            and build infastructure, we can provide fast bus stops all over
-            indeanapolis. With your input we can return accurate bus stops
-            accordingto your coordinats
+          <p className="lorem-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque consequat nisi id risus malesuada, vitae commodo nulla
+            efficitur. Sed at volutpat leo, at fermentum neque. Aliquam erat
+            volutpat. Fusce auctor sapien a mauris ullamcorper, a interdum elit
+            vehicula. Phasellus in justo sit amet odio sodales vehicula sit amet
+            eget magna. Ut vel ligula vitae turpis dignissim lacinia. Curabitur
+            sit amet eros ut lacus aliquam blandit. Sed ut arcu purus.
+            Suspendisse potenti. Morbi non metus nulla. Donec dictum fringilla
+            nisl, eget consectetur felis tempus id. Cras et
           </p>
           <br></br>
           <img
@@ -137,9 +144,9 @@ export default function SponsorRegister() {
         <div className="aboutPic">
           <br></br>
           <img
-            style={{ width: '30%' }}
-            src='https://st2.depositphotos.com/3765139/6173/i/950/depositphotos_61730885-stock-photo-old-bus-retro-style2.jpg'
-            alt='IndyGo Bus'
+            style={{ width: "30%" }}
+            src="https://st2.depositphotos.com/3765139/6173/i/950/depositphotos_61730885-stock-photo-old-bus-retro-style2.jpg"
+            alt="IndyGo Bus"
           />
           <br></br>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
@@ -147,16 +154,13 @@ export default function SponsorRegister() {
           at volutpat leo, at fermentum neque. Aliquam erat volutpat. Fusce
           auctor sapien a mauris ullamcorper, a interdum elit vehicula.
           Phasellus in justo sit amet odio sodales vehicula sit amet eget magna.
-          Ut vel ligula vitae turpis dignissim lacinia. Curabitur sit amet eros
-          ut lacus aliquam blandit. Sed ut arcu purus. Suspendisse potenti.
-          Morbi non metus nulla. Donec dictum fringilla nisl, eget consectetur
-          felis tempus id. Cras et
+          Ut vel ligula vitae turpis dignissim lacinia.
         </div>
         <br></br>
         <img
-          style={{ width: '30%' }}
-          src='https://st2.depositphotos.com/3765139/6173/i/950/depositphotos_61730885-stock-photo-old-bus-retro-style2.jpg'
-          alt='IndyGo Bus'
+          style={{ width: "30%" }}
+          src="https://st2.depositphotos.com/3765139/6173/i/950/depositphotos_61730885-stock-photo-old-bus-retro-style2.jpg"
+          alt="IndyGo Bus"
         />
         <br></br>
         <div className="randomParagraph">
@@ -175,8 +179,8 @@ export default function SponsorRegister() {
         <br></br>
         <div className="contact-pic">
           <img
-            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1MItdHg7Dft465SwXLUCH5IkW6bwP53V4LduwJQhMDI_-jDbmKhihTQqtIcicppNOoyo&usqp=CAU'
-            alt='IndyGo Bus'
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1MItdHg7Dft465SwXLUCH5IkW6bwP53V4LduwJQhMDI_-jDbmKhihTQqtIcicppNOoyo&usqp=CAU"
+            alt="IndyGo Bus"
           />
         </div>
         <br></br>
@@ -191,22 +195,9 @@ export default function SponsorRegister() {
           perspiciatis unde omnis iste natus error sit voluptatem accusantium
           doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
           inventore veritatis et quasi architecto beatae vitae dicta sunt
-          explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-          odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-          voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-          quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-          eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-          voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
-          ullam corporis suscipit laboriosam, nisi ut
+          explicabo.
         </p>
       </div>
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem.
-      </p>
     </>
-  )
-};
+  );
+}
