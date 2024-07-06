0574997e05527322e979e8a555a9d7d19e2e8bcc
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-07-06 19:51:24 -0400

    "commented out home element"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,14 @@ import Contact from "./components/Contact";
 import Sponsor from "./components/Sponsor";
 import Footer from "./components/Footer";
 import Map from "./components/Map";
+// import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/contact" element={<Contact />} />
