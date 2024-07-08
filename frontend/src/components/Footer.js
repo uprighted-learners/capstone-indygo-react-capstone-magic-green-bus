1adc61fa851265d10b622fa91a4e1a8ac45b9885
qlansb <qlansbury14@gmail.com>
2024-07-07 21:21:42 -0400

    made it so opening a link will open a new tab

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,11 +10,11 @@ function Footer() {
       </p>
       <p className='Footer-Creators'>Site creators:</p>
       <ul className="footer-links">
-        <li><a href="https://www.linkedin.com/in/quinn-lansbury-6a5a452b5/">Quinn</a></li>
-        <li><a href="https://www.linkedin.com/in/ryan-fish-15791517b/">Ryan</a></li>
-        <li><a href="https://www.linkedin.com/in/ivana-carrillo-8b1225293">Ivana</a></li>
-        <li><a href="https://www.linkedin.com/in/alex-aubin-5740602a7/">Alex</a></li>
-        <li><a href="https://www.linkedin.com/in/joe-reis-software-developer-b289802a2/">Joe</a></li>
+        <li><a href="https://www.linkedin.com/in/quinn-lansbury-6a5a452b5/" target="_blank" rel="noopener noreferrer">Quinn</a></li>
+        <li><a href="https://www.linkedin.com/in/ryan-fish-15791517b/" target="_blank" rel="noopener noreferrer">Ryan</a></li>
+        <li><a href="https://www.linkedin.com/in/ivana-carrillo-8b1225293" target="_blank" rel="noopener noreferrer">Ivana</a></li>
+        <li><a href="https://www.linkedin.com/in/alex-aubin-5740602a7/" target="_blank" rel="noopener noreferrer">Alex</a></li>
+        <li><a href="https://www.linkedin.com/in/joe-reis-software-developer-b289802a2/" target="_blank" rel="noopener noreferrer">Joe</a></li>
       </ul>
     </footer>
   );
