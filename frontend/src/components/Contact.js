7be0ece2ac222fa0603108a4f5e538b9906be9c2
JoeR0814 <joereis0814@gmail.com>
2024-07-09 11:14:39 -0400

    finsished stying three pictures in the contact file

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -6,16 +6,18 @@ export default function Contact() {
     <>
       <div className='contact-container'>
         <div className='contact-content'>
-          <img
-            className='security-image'
-            src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017993316831324/e11b8411-a122-4d43-9fa9-ea3898675109.jpg?ex=668dcab3&is=668c7933&hm=044f95ab5f507e270ed84f26989e3fea3bbaba564bf39ad192f3a2a1e0f15dcb&'
-            alt=''
-          ></img>
-          <img
-            className='bus-image'
-            src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017403811725342/1cdc7c55-56f8-4759-97f2-dfdf370c5ba5.jpg?ex=668dca27&is=668c78a7&hm=6041e0c68603076bdb1fd5aa74be13ea495040645068d22b0abe56f47a4106ca&'
-            alt=''
-          ></img>
+          <div className='image-container'>
+            <img
+              className='security-image'
+              src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017993316831324/e11b8411-a122-4d43-9fa9-ea3898675109.jpg?ex=668dcab3&is=668c7933&hm=044f95ab5f507e270ed84f26989e3fea3bbaba564bf39ad192f3a2a1e0f15dcb&'
+              alt=''
+            ></img>
+            <img
+              className='bus-image'
+              src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017403811725342/1cdc7c55-56f8-4759-97f2-dfdf370c5ba5.jpg?ex=668dca27&is=668c78a7&hm=6041e0c68603076bdb1fd5aa74be13ea495040645068d22b0abe56f47a4106ca&'
+              alt=''
+            ></img>
+          </div>
           <p>
             For any security/safety questions or concerns, please use the
             contact button below.
