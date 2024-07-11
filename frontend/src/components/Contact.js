9fea085d7f6d6789fcfb485a21bed6ea807c1d8b
RyanF8 <Ryanfish4229@gmail.com>
2024-07-11 12:24:01 -0400

    fixed images on contact page

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Contact.css';
+import image1 from '../publicphotos/Assets/contact-page-image-1.png';
+import image2 from '../publicphotos/Assets/contact-page-image-2.png';
+import image3 from '../publicphotos/Assets/contact-page-image-3.png';
 
 export default function Contact() {
   return (
@@ -9,13 +12,13 @@ export default function Contact() {
           <div className='image-container'>
             <img
               className='security-image'
-              src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017993316831324/e11b8411-a122-4d43-9fa9-ea3898675109.jpg?ex=668dcab3&is=668c7933&hm=044f95ab5f507e270ed84f26989e3fea3bbaba564bf39ad192f3a2a1e0f15dcb&'
-              alt=''
+              src={image1}
+              alt='lock pc screen'
             ></img>
             <img
               className='bus-image'
-              src='https://cdn.discordapp.com/attachments/1259668960912212029/1260017403811725342/1cdc7c55-56f8-4759-97f2-dfdf370c5ba5.jpg?ex=668dca27&is=668c78a7&hm=6041e0c68603076bdb1fd5aa74be13ea495040645068d22b0abe56f47a4106ca&'
-              alt=''
+              src={image2}
+              alt='yellow bus'
             ></img>
           </div>
           <p>
@@ -27,8 +30,8 @@ export default function Contact() {
 
           <img
             className='contact-image'
-            src='https://cdn.discordapp.com/attachments/1259668960912212029/1260016300894322738/6479a0f6-1ff5-4016-afe3-758b5b67558c.jpg?ex=668dc920&is=668c77a0&hm=a21ac55f22111a917e8c662f518fd2aee2e7c8326b95970048e0517aef87a7fa&'
-            alt=''
+            src={image3}
+            alt='information desk'
           ></img>
           <p>
             For any website related issues please contact William by clicking
